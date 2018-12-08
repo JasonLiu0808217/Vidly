cde51c49fecdc4a85f0ef4e2647e19225b2d5fa0
Jason Liu <jason.liu0808217@gmail.com>
2018-12-07 23:36:15 -0500

    Custom Route

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -14,6 +14,33 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() { Name = "Shrek!" };
             return View(movie);
+            // return Content("Hellow World");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home",new { page = 1, sortby = "name"});
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            return Content("id=" + Id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortby)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (string.IsNullOrWhiteSpace(sortby))
+            {
+                sortby = "Name";
+            }
+            return Content(string.Format("pageIndex = {0}&sortBy = {1}", pageIndex, sortby));
         }
     }
 }
